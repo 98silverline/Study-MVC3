a5dfe57eef3d97fb2a04c68486c9c98d24fb56b3
98silverline <98silverline@gmail.com>
2024-10-29 21:33:19 +0900

    타임리프 적용 완료

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -33,7 +33,7 @@ public class ItemRepository {
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
         findItem.setOpen(updateParam.getOpen());
-        findItem.setRegisons(updateParam.getRegisons());
+        findItem.setRegions(updateParam.getRegions());
         findItem.setItemType(updateParam.getItemType());
         findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
